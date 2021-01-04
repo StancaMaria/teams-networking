45af53f7d93b324a005a9668395dc67d401f4315
Stanca Stirb <stancastirb@gmail.com>
2021-01-04 20:50:11 +0200

    add

--- functions.js
+++ functions.js
@@ -1,3 +1,22 @@
+const API = {
+    CREATE: {
+        URL: "create.json",
+        METHOD: "GET" // POST
+    },
+    READ: {
+        URL: "team.json",
+        METHOD: "GET"
+    },
+    UPDATE: {
+        URL: "",
+        METHOD: "GET"
+    },
+    DELETE: {
+        URL: "",
+        METHOD: "GET"
+    }
+};
+
 function insertPersons(persons) {
     const tbody = document.querySelector('#list tbody');
     tbody.innerHTML = getPersonsHtml(persons);
@@ -13,31 +32,70 @@ function getPersonHtml (person) {
         <td>${person.firstName}</td>
         <td>${person.lastName}</td>
         <td><a target="_blank" href="https://github.com/${gitHub}">Github</a></td>
+        <td></td>
     </tr>`;
 }
 
 let allPersons = [];
 
-fetch('team.json')
-  .then(res => res.json())
-    .then(data => {
-        allPersons = data;
-        insertPersons(data);
-  });
+function loadList() {
+    fetch(API.READ.URL)
+        .then(res => res.json())
+        .then(data => {
+            allPersons = data;
+            insertPersons(data);
+        });
+}
+
+loadList();
 
 function searchPersons(text) {
-    console.warn("search", text, allPersons);
+    text = text.toLowerCase();
+    console.warn("search", text);
     return allPersons.filter(person => {
-        console.info(person.firstName);
-        return person.firstName.indexOf(text) != -1;
+        return person.firstName.toLowerCase().indexOf(text) > -1 ||
+            person.lastName.toLowerCase().indexOf(text) > -1;
     });
-  }
-    
+}
+
 const search = document.getElementById('search');
 search.addEventListener("input", e => {
     const text = e.target.value;
-  
-    const filtrare = searchPersons(text);
 
-    insertPersons(filtrare)
-})
+    const filtrate = searchPersons(text);
+    console.info({filtrate})
+
+    insertPersons(filtrate);
+});
+
+function saveTeamMember() {
+    const firstName = document.querySelector("#list input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+    
+    const person = {
+        firstName,
+        lastName,
+        gitHub: gitHub
+    };
+    console.info('saving...', person, JSON.stringify(person));
+
+    fetch(API.CREATE.URL, {
+        method: API.CREATE.METHOD,
+        body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+        .then(res => res.json())
+        .then(r => {
+            console.warn(r);
+            if (r.success) {
+                alert('saving data..., please wait until we are ready.');
+                console.info('refresh list');
+                loadList();
+            }
+        });
+}
+
+const saveBtn = document.querySelector("#list tfoot button");
+saveBtn.addEventListener("click", () => {
+    saveTeamMember();
+});
