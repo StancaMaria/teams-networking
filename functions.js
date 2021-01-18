5afd2a74cda260610524475ccf58e9aba0f89cc1
Stanca Stirb <stancastirb@gmail.com>
2021-01-18 21:19:54 +0200

    add my things

--- functions.js
+++ functions.js
@@ -1,21 +1,23 @@
 const API = {
     CREATE: {
-        URL: "create.json",
-        METHOD: "GET" // POST
+        URL: "http://localhost:3000/teams-json/create",
+        METHOD: "POST"
     },
     READ: {
-        URL: "team.json",
+        URL: "http://localhost:3000/teams-json",
         METHOD: "GET"
     },
     UPDATE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "http://localhost:3000/teams-json/update",
+        METHOD: "PUT"
     },
     DELETE: {
-        URL: "",
-        METHOD: "GET"
+        URL: "http://localhost:3000/teams-json/delete",
+        METHOD: "DELETE"
     }
 };
+
+let editId;
 
 function insertPersons(persons) {
     const tbody = document.querySelector('#list tbody');
@@ -33,7 +35,8 @@ function getPersonHtml (person) {
         <td>${person.lastName}</td>
         <td><a target="_blank" href="https://github.com/${gitHub}">Github</a></td>
         <td>
-        <a href="${API.DELETE.URL}?id=${person.id}">&#10006;</a>
+            <a href="#" class="delete-row" data-id="${person.id}">&#10006;</a>
+            <a href="#" class="edit-row" data-id="${person.id}">&#9998;</a>
         </td>
     </tr>`;
 }
@@ -60,16 +63,6 @@ function searchPersons(text) {
     });
 }
 
-const search = document.getElementById('search');
-search.addEventListener("input", e => {
-    const text = e.target.value;
-
-    const filtrate = searchPersons(text);
-    console.info({filtrate})
-
-    insertPersons(filtrate);
-});
-
 function saveTeamMember() {
     const firstName = document.querySelector("#list input[name=firstName]").value;
     const lastName = document.querySelector("input[name=lastName]").value;
@@ -84,20 +77,110 @@ function saveTeamMember() {
 
     fetch(API.CREATE.URL, {
         method: API.CREATE.METHOD,
+        headers: {
+            "Content-Type": "application/json"
+        },
         body: API.CREATE.METHOD === "GET" ? null : JSON.stringify(person)
     })
         .then(res => res.json())
         .then(r => {
             console.warn(r);
             if (r.success) {
-                alert('saving data..., please wait until we are ready.');
-                console.info('refresh list');
                 loadList();
             }
         });
 }
 
-const saveBtn = document.querySelector("#list tfoot button");
-saveBtn.addEventListener("click", () => {
-    saveTeamMember();
-});
+function updateTeamMember() {
+    const firstName = document.querySelector("#list input[name=firstName]").value;
+    const lastName = document.querySelector("input[name=lastName]").value;
+    const gitHub = document.querySelector("input[name=gitHub]").value;
+    
+    const person = {
+        id: editId,
+        firstName,
+        lastName,
+        gitHub: gitHub
+    };
+    console.info('updating...', person, JSON.stringify(person));
+
+    fetch(API.UPDATE.URL, {
+        method: API.UPDATE.METHOD,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: API.UPDATE.METHOD === "GET" ? null : JSON.stringify(person)
+    })
+        .then(res => res.json())
+        .then(r => {
+            console.warn(r);
+            if (r.success) {
+                loadList();
+            }
+        });
+}
+
+function deleteTeamMember(id) {
+    fetch(API.DELETE.URL, {
+        method: API.DELETE.METHOD,
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ id })
+    })
+        .then(r => r.json())
+        .then(r => {
+            if (r.success) {
+                loadList();
+            }
+        });
+}
+
+function populateCurrentMember(id) {
+    var person = allPersons.find(person => person.id === id);
+
+    editId = id;
+    
+    const firstName = document.querySelector("#list input[name=firstName]");
+    const lastName = document.querySelector("input[name=lastName]");
+    const gitHub = document.querySelector("input[name=gitHub]");
+
+    firstName.value = person.firstName;
+    lastName.value = person.lastName;
+    gitHub.value = person.gitHub;
+}
+
+function addEventListeners() {
+    const search = document.getElementById('search');
+    search.addEventListener("input", e => {
+        const text = e.target.value;
+    
+        const filtrate = searchPersons(text);
+        console.info({filtrate})
+    
+        insertPersons(filtrate);
+    });
+    
+    const saveBtn = document.querySelector("#list tfoot button");
+    saveBtn.addEventListener("click", () => {
+        if (editId) {
+            updateTeamMember();
+        } else {
+            saveTeamMember();
+        }
+    });
+
+    const table = document.querySelector("#list tbody");
+    table.addEventListener("click", (e) => {
+        const target = e.target;
+        if (target.matches("a.delete-row")) {
+            const id = target.getAttribute("data-id");
+            deleteTeamMember(id);
+        } else if (target.matches("a.edit-row")) {
+            const id = target.getAttribute("data-id");
+            populateCurrentMember(id);
+        }
+    });
+}
+
+addEventListeners();
